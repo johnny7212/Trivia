f3408a43e8885939e6cbf8ca9c27907dd651d5fc
johnny7212 <jonddueno@gmail.com>
2022-12-19 13:12:01 -0600

    Bingus

--- src/Questions.java
+++ src/Questions.java
@@ -1,95 +1,39 @@
 public class Questions {
-    //MAKE NEW ARRAY FOR THE POINTS PER QUESTION
+
+    String question;
+    String answer;
+    int pointVal;
+
+    public Questions(String question, String answer, int pointVal) {
+        this.question = question;
+        this.answer = answer;
+        this.pointVal = pointVal;
+    }
 
     public static String [] questions = {
-            "What game did mario first appear in?(5)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What year is the first game believed to have been created(5)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What is the best selling console that is said to have sold 155 million worldwide?(4)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What video game was promoted in a commercial featuring the song “Happy Together” by the Turtles?(5)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What orange furry character was a mascot for Playstation?(3)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What is the highest selling X-box game?(3)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What year was the first X-box console released?(3)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "Which fighting game was released first, Street Fighter or Tekken?(3)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What handheld gaming system is the best-selling of all time?(2)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What’s the most expensive video game ever produced ($1000)?(4)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "What was the last Sega console?(5)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
-            "Who is Jack Mitchell’s best friend in “Call of Duty: Advanced Warfare?”(8)" +
-                    "\n\t a." +
-                    "\n\t b." +
-                    "\n\t c." +
-                    "\n\t d.",
+            new Questions("What game did mario first appear in?(5)", "Donkey Kong", 5).toString(),
+            new Questions("What year is the first game believed to have been created(5)", "1958", 5).toString(),
+            new Questions("What is the best-selling console that is said to have sold 155 million worldwide?(4)", "Playstation 2", 4).toString(),
+            new Questions("What video game was promoted in a commercial featuring the song “Happy Together” by the Turtles?(5)", "Super Smash Brothers",5).toString(),
+            new Questions("What orange furry character was a mascot for Playstation?(3)", "Crash Bandicoot",3).toString(),
+            new Questions("What is the highest selling X-box game?(3)", "Halo 2",3).toString(),
+            new Questions("What year was the first X-box console released?(3)", "2001",3).toString(),
+            new Questions("Which fighting game was released first, Street Fighter or Tekken?(3)", "Street Fighter",3).toString(),
+            new Questions("What handheld gaming system is the best-selling of all time?(2)", "Nintendo Ds",2).toString(),
+            new Questions("What’s the most expensive video game ever produced [$1000]?(4)", "Star Citizen",4).toString(),
+            new Questions("What was the last Sega console?(5)", "Sega Dreamcast",5).toString(),
+            new Questions("Who is Jack Mitchell’s best friend in “Call of Duty: Advanced Warfare?”(8)", "Will Irons",8).toString()
+
     };
 
-    public static int [] answerPoints = {
-        5, 5, 4, 5, 3, 3, 3, 3, 2, 4, 5, 8
-    };
-
-    public static String [] answers = {
-            "Donkey Kong",
-            "1958",
-            "Playstation 2",
-            "Super Smash Brothers",
-            "Crash Bandicoot",
-            "Halo 2",
-            "2001",
-            "Street Fighter",
-            "Nintendo DS",
-            "Star Citizen",
-            "SEGA Dreamcast",
-            "Will Irons"
-    };
-
-
-
-    public String toString(){
-        return Questions.answers[0] ;
+    public String getQuestion(){
+        return question;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public int getPointVal(){
+        return pointVal;
     }
 
-    public static void main(String[] args) {
-
-    }
 }
--- src/TriviaGame.java
+++ src/TriviaGame.java
@@ -1,21 +1,34 @@
+import java.util.Random;
+
 public class TriviaGame {
-    private static int pointTotal = 50;
-    public static int userTotal = 0;
+    private static int userStreak = 0;
+    private static int userTotal = 0;
+    private static int numCorrect = 0;
 
     public static void trivia() {
-        for (int i = 0; i < Questions.questions.length; i++){
-            System.out.println(Questions.questions[i]);
-            String answer = TriviaDriver.scanner.nextLine();
-            if (answer.equals(Questions.answers[i])){
-                System.out.println("Correct!");
+        Random random = new Random();
 
-                userTotal += Questions.answerPoints[i];
+        for (int i = 0; i < Questions.questions.length; i++){
+            System.out.println("Question #" + (i+1));
+            System.out.println(Questions.questions[i].);
+            String answer = TriviaDriver.scanner.nextLine();
+            if (answer.equals(Questions.questions[i].)){
+                System.out.println("Correct!");
+                userStreak++;
+                numCorrect++;
+                userTotal += Questions.;
             } else {
                 System.out.println("Incorrect!");
+                userStreak = 0;
             }
-            System.out.println("Current Points: " + userTotal);
+            System.out.println("Current Points: " + userTotal + "\n Current Streak: " + userStreak);
         }
+        double getScore = userTotal / 50;
+        System.out.println("Overall Stats: " +
+                "\n You got " + numCorrect + " questions out of 12 correct!" +
+                "\n Congrats! You got a " + getScore + "% score!");
     }
+
 
     public static void main(String[] args) {
         trivia();
